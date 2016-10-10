1f357d0da615df39745cfefd6949cdee6b80f35d
alu0100406122 <alu0100406122@ull.edu.es>
2016-10-10 11:57:30 +0000

    Añadiendo tareas para que el usuario sincronice con su repositorio.

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
-var watch = require('gulp-watch');
 var install = require('gulp-install');
+var git = require('gulp-git');
+var path = require('path');
+var json = require(path.join(__dirname,'package.json'));
 
 gulp.task('construir_gitbook', function()
 {
@@ -21,6 +23,61 @@ gulp.task('construir_gitbook', function()
         .pipe(shell([
             "./scripts/losh deploy-wiki"    
         ]))
+});
+
+// Creando repositorio
+
+gulp.task('init', function(){
+   git.init(function(err){
+       if(err) throw err;
+   
+       console.log("Repositorio inicializado.");
+   });
+   
+});
+
+gulp.task('addremote', function(){
+   git.addRemote('origin', json.repository.url, function(err){
+       if(err) throw err;
+       
+       console.log("Repositorio remoto añadido correctamente.");
+   });
+});
+
+
+// Push inicial
+
+gulp.task('push_inicial', ['init', 'addremote'], function(){
+    git.push('origin', 'master', function(err){
+       if(err) 
+       {
+           console.log("err:"+err);
+           throw err; 
+       }
+    });
+});
+
+
+//Actualizar repositorio
+
+gulp.task('git-add', function(){
+    return gulp.src('')
+               .pipe(git.add());
+});
+
+gulp.task('git-commit', function(){
+    return gulp.src('')
+               .pipe(git.commit('Actualizando Gitbook'));
+});
+
+gulp.task('push', ['git-add','git-commit'], function(){
+    git.push('origin', 'master', function(err){
+       if(err) 
+       {
+           console.log("err:"+err);
+           throw err; 
+       }
+    });
 });
 
 gulp.task('instalar_recursos',['instalar_dependencias','instalar_plugins']);
