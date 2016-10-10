a27aa8fd16feb3c8447e4bb004d2d76984a23a56
alu0100406122 <alu0100406122@ull.edu.es>
2016-10-10 11:06:18 +0000

    Valores por defecto para el autor del libro: caso user.github.

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+"use strict"
 
 const fs = require('fs-extra');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const path = require('path');
 const basePath = process.cwd();
 const myArgs = require('minimist')(process.argv.slice(2));
 const json = require(path.join(__dirname,'../package.json')); 
+const gitconfig = require('git-config');
+
 
 var directorio;
 var autor;
@@ -33,108 +36,120 @@ if(myArgs.h || myArgs.help)
 }
 else
 {
-    
     //Comprobando las opciones
     
-    directorio = myArgs.d || myArgs.name || 'Milibro';
-    autor = myArgs.autor || "Usuario";
-    nombre_gitbook = myArgs.name || myArgs.d || myArgs.autor || "Milibro";
+    gitconfig(function(err,config){
+        if(err) console.error(err);
+        // git_user = config.user.name;
+        // console.log("git_user: "+config.user.name);
+        autor = myArgs.autor || config.user.name || "Usuario";  
+        console.log("git_user: "+config.user.name);
+        //Comprobando las opciones
+          
     
-    if(myArgs.url)
-    {
-        url_repo = myArgs.url;
-        url_wiki = myArgs.url.split(".git")[0].concat('.wiki.git');
-        url_bugs = myArgs.url.split(".git")[0].concat('/issues');
-        // console.log("Split:"+url_wiki);
-    }
-    else
-    {
-        //Aqui podriamos crear un repo a través de la api de github
-        url_repo = " ";
-        url_wiki = " ";
-        url_bugs = " ";
-    }
-    
-    fs.mkdirp(path.join(basePath, directorio), function(err){
-        if(err){
-            console.error(err);
+        directorio = myArgs.d || myArgs.name || 'Milibro';
+        nombre_gitbook = myArgs.name || myArgs.d || myArgs.autor || "Milibro";
+        
+        if(myArgs.url)
+        {
+            url_repo = myArgs.url;
+            url_wiki = myArgs.url.split(".git")[0].concat('.wiki.git');
+            url_bugs = myArgs.url.split(".git")[0].concat('/issues');
+            // console.log("Split:"+url_wiki);
         }
-        else{
-    
-          fs.copy(path.join(__dirname,'../template','gulpfile.js'), path.join(basePath, directorio , 'gulpfile.js'));
-    
-          fs.copy(path.join(__dirname,'../template','README.md'), path.join(basePath, directorio , 'README.md'));   
-    
-          fs.copy(path.join(__dirname, '../template', 'VERSION'), path.join(basePath, directorio , 'VERSION'));   
-           
-          fs.mkdirp(path.join(basePath, directorio , 'scripts'), function (err) {
-              if (err) {
+        else
+        {
+            //Aqui podriamos crear un repo a través de la api de github
+            url_repo = " ";
+            url_wiki = " ";
+            url_bugs = " ";
+        }
+        
+        fs.mkdirp(path.join(basePath, directorio), function(err){
+            if(err){
                 console.error(err);
-              }
-          });
-           
-            fs.copy(path.join(__dirname, '../template', 'scripts') , path.join(basePath, directorio ,'scripts'), function(err){
-                if(err) return console.error(err)
-            });  
-           
-          fs.mkdirp(path.join(basePath, directorio , '/txt'), function (err) {
-              if (err) {
-                console.error(err);
-              }
-              else {
-    
-                fs.copy(path.join(__dirname, '../template', 'txt' , 'SUMMARY.md'), path.join(basePath, directorio , 'txt', 'SUMMARY.md'));
-    
-                fs.copy(path.join(__dirname,'../template', 'txt', 'section1'), path.join(basePath, directorio , 'txt', 'section1'), function(err){
+            }
+            else{
+        
+              fs.copy(path.join(__dirname,'../template','gulpfile.js'), path.join(basePath, directorio , 'gulpfile.js'));
+        
+              fs.copy(path.join(__dirname,'../template','README.md'), path.join(basePath, directorio , 'README.md'));   
+        
+              fs.copy(path.join(__dirname, '../template', 'VERSION'), path.join(basePath, directorio , 'VERSION'));   
+               
+              fs.mkdirp(path.join(basePath, directorio , 'scripts'), function (err) {
+                  if (err) {
+                    console.error(err);
+                  }
+              });
+               
+                fs.copy(path.join(__dirname, '../template', 'scripts') , path.join(basePath, directorio ,'scripts'), function(err){
                     if(err) return console.error(err)
-    
-                });   
-                
-                ejs.renderFile(path.join(__dirname, '../template', 'txt', 'README.ejs'), { name_gitbook: nombre_gitbook}, function(err,str) {
-                  if(err) {
-                      console.error(err);
-                      throw err;
+                });  
+               
+              fs.mkdirp(path.join(basePath, directorio , '/txt'), function (err) {
+                  if (err) {
+                    console.error(err);
                   }
                   else {
-                      if(str) {
-    
-                          //Creamos y escribimos en el fichero README.md
-                          fs.writeFile(path.join(basePath, directorio ,'txt', 'README.md'), str);
+        
+                    fs.copy(path.join(__dirname, '../template', 'txt' , 'SUMMARY.md'), path.join(basePath, directorio , 'txt', 'SUMMARY.md'));
+        
+                    fs.copy(path.join(__dirname,'../template', 'txt', 'section1'), path.join(basePath, directorio , 'txt', 'section1'), function(err){
+                        if(err) return console.error(err)
+        
+                    });   
+                    
+                    ejs.renderFile(path.join(__dirname, '../template', 'txt', 'README.ejs'), { name_gitbook: nombre_gitbook}, function(err,str) {
+                      if(err) {
+                          console.error(err);
+                          throw err;
                       }
+                      else {
+                          if(str) {
+        
+                              //Creamos y escribimos en el fichero README.md
+                              fs.writeFile(path.join(basePath, directorio ,'txt', 'README.md'), str);
+                          }
+                      }
+                    });
+        
                   }
                 });
-    
-              }
-            });
-        }
-    });
-    
-    // Construyendo "package.json"
-    
-    ejs.renderFile(path.join(__dirname, '../template', 'package.ejs'), { autor: autor , name_gitbook: nombre_gitbook, url: url_repo, url_bugs: url_bugs, url_wiki: url_wiki}, function(err,str){
-        if(err) {
-            console.error("ERROR:"+err);
-        }
-        if(str) {
+            }
+        });
+        
+        // Construyendo "package.json"
+        
+        ejs.renderFile(path.join(__dirname, '../template', 'package.ejs'), { autor: autor , name_gitbook: nombre_gitbook, url: url_repo, url_bugs: url_bugs, url_wiki: url_wiki}, function(err,str){
+            if(err) {
+                console.error("ERROR:"+err);
+            }
+            if(str) {
+                
+                fs.writeFile(path.join(basePath, directorio , 'package.json'), str);
+            }
             
-            fs.writeFile(path.join(basePath, directorio , 'package.json'), str);
-        }
+        });
+        
+        // Construyendo "book.json"
+        
+        ejs.renderFile(path.join(__dirname, '../template', 'book.ejs'), { name_gitbook: nombre_gitbook}, function(err,str){
+            if(err) {
+                console.error("ERROR:"+err);
+            }
+            if(str) {
+        
+                fs.writeFile(path.join(basePath, directorio , 'book.json'), str);
+        
+            }
+            
+        });
+        
+        console.log("Gitbook built!");
+    
         
     });
+      
     
-    // Construyendo "book.json"
-    
-    ejs.renderFile(path.join(__dirname, '../template', 'book.ejs'), { name_gitbook: nombre_gitbook}, function(err,str){
-        if(err) {
-            console.error("ERROR:"+err);
-        }
-        if(str) {
-    
-            fs.writeFile(path.join(basePath, directorio , 'book.json'), str);
-    
-        }
-        
-    });
-    
-    console.log("Gitbook built!");
 }
