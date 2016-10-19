2155bad0f307fa37650a02cbb141fe0c56228e13
josuetc94 <alu0100763492@ull.edu.es>
2016-10-19 19:37:40 +0000

    Creación de dir src. Exportando librerías. Ajustando deploy-iaas

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 "use strict"
 
-const fs = require('fs-extra');
-const ejs = require('ejs');
+// const fs = require('fs-extra');
+// const ejs = require('ejs');
 const path = require('path');
-const basePath = process.cwd();
+// const basePath = process.cwd();
 const myArgs = require('minimist')(process.argv.slice(2));
 const json = require(path.join(__dirname,'../package.json')); 
-const gitconfig = require('git-config');
+// const gitconfig = require('git-config');
 const iaas = require('gitbook-start-iaas-ull-es-josue-nayra');
+const creacion = require(path.join(__dirname,'../src/creacion_gitbook.js'));
 
-var directorio;
-var autor;
-var url_repo;
-var nombre_gitbook;
-var url_wiki;
-var url_bugs;
+// var directorio;
+// var autor;
+// var url_repo;
+// var nombre_gitbook;
+// var url_wiki;
+// var url_bugs;
 
 
 // switch(myArgs.help | myArgs.version | myArgs.deploy){
@@ -45,117 +46,6 @@ var url_bugs;
 // }
 
 
-
-var crear_gitbook = (() => {
-        gitconfig(function(err,config){
-            if(err) console.error(err);
-    
-            autor = myArgs.autor || config.user.name || "Usuario"; 
-            directorio = myArgs.d || myArgs.dir || myArgs.name || 'Milibro';
-            nombre_gitbook = myArgs.name || myArgs.d || myArgs.autor || "Milibro";
-            
-            if(myArgs.url)
-            {
-                url_repo = myArgs.url;
-                url_wiki = myArgs.url.split(".git")[0].concat('.wiki.git');
-                url_bugs = myArgs.url.split(".git")[0].concat('/issues');
-                // console.log("Split:"+url_wiki);
-            }
-            else
-            {
-                //Aqui podriamos crear un repo a través de la api de github
-                url_repo = " ";
-                url_wiki = " ";
-                url_bugs = " ";
-            }
-            
-            fs.mkdirp(path.join(basePath, directorio), function(err){
-                if(err){
-                    console.error(err);
-                }
-                else{
-            
-                  fs.copy(path.join(__dirname,'../template','gulpfile.js'), path.join(basePath, directorio , 'gulpfile.js'));
-            
-                  fs.copy(path.join(__dirname,'../template','README.md'), path.join(basePath, directorio , 'README.md'));   
-            
-                  fs.copy(path.join(__dirname, '../template', 'VERSION'), path.join(basePath, directorio , 'VERSION'));   
-                   
-                  fs.mkdirp(path.join(basePath, directorio , 'scripts'), function (err) {
-                      if (err) {
-                        console.error(err);
-                      }
-                  });
-                   
-                    fs.copy(path.join(__dirname, '../template', 'scripts') , path.join(basePath, directorio ,'scripts'), function(err){
-                        if(err) return console.error(err)
-                    });  
-                   
-                  fs.mkdirp(path.join(basePath, directorio , '/txt'), function (err) {
-                      if (err) {
-                        console.error(err);
-                      }
-                      else {
-            
-                        fs.copy(path.join(__dirname, '../template', 'txt' , 'SUMMARY.md'), path.join(basePath, directorio , 'txt', 'SUMMARY.md'));
-            
-                        fs.copy(path.join(__dirname,'../template', 'txt', 'section1'), path.join(basePath, directorio , 'txt', 'section1'), function(err){
-                            if(err) return console.error(err)
-            
-                        });   
-                        
-                        ejs.renderFile(path.join(__dirname, '../template', 'txt', 'README.ejs'), { name_gitbook: nombre_gitbook}, function(err,str) {
-                          if(err) {
-                              console.error(err);
-                              throw err;
-                          }
-                          else {
-                              if(str) {
-            
-                                  //Creamos y escribimos en el fichero README.md
-                                  fs.writeFile(path.join(basePath, directorio ,'txt', 'README.md'), str);
-                              }
-                          }
-                        });
-            
-                      }
-                    });
-                }
-            });
-            
-            // Construyendo "package.json"
-            
-            ejs.renderFile(path.join(__dirname, '../template', 'package.ejs'), { autor: autor , name_gitbook: nombre_gitbook, url: url_repo, url_bugs: url_bugs, url_wiki: url_wiki}, function(err,str){
-                if(err) {
-                    console.error("ERROR:"+err);
-                }
-                if(str) {
-                    
-                    fs.writeFile(path.join(basePath, directorio , 'package.json'), str);
-                }
-                
-            });
-            
-            // Construyendo "book.json"
-            
-            ejs.renderFile(path.join(__dirname, '../template', 'book.ejs'), { name_gitbook: nombre_gitbook}, function(err,str){
-                if(err) {
-                    console.error("ERROR:"+err);
-                }
-                if(str) {
-            
-                    fs.writeFile(path.join(basePath, directorio , 'book.json'), str);
-            
-                }
-                
-        });
-    });
-
-    console.log("Gitbook built!");
-    
-});
-
-
 if(myArgs.h || myArgs.help)
 {
     console.log("Seccion de ayuda.");
@@ -179,12 +69,11 @@ else
         if(myArgs.deploy)
         {
             console.log("Deploy to Iaas.");
-            iaas.deploy(myArgs.IP, myArgs.path, url_repo);
+            iaas.deploy(myArgs.IP, myArgs.path, json.repository.url);
         }
         else
         {
-            crear_gitbook();   
+            creacion.crear_gitbook();   
         }
     }
 }
-    //Comprobando las opciones
---
+++ chuchu/gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var install = require('gulp-install');
+var path = require('path');
+var json = require(path.join(__dirname,'package.json'));
+var git = require('simple-git');
+var fs = require('fs-extra');
+
+
+//------------------------------------------------------------------------------------
+// Repositorio Github
+
+gulp.task('push', function(){
+    if (!fs.existsSync(path.join(__dirname, '.git'))){
+      git()
+        .init()
+        .add('./*')
+        .commit("first commit")
+        .addRemote('origin', json.repository.url)
+        .push('origin', 'master');
+    }
+    else
+    {
+       git()
+        .add('./*')
+        .commit("Actualizando Gitbook.")
+        .push('origin', 'master');   
+    }
+});
+
+//------------------------------------------------------------------------------------
+// Instalar dependencias y recursos
+
+gulp.task('instalar_recursos',['instalar_dependencias','instalar_plugins']);
+
+gulp.task('instalar_dependencias', function()
+{
+    gulp.src(['./package.json']).pipe(install())
+});
+
+gulp.task('instalar_plugins', function()
+{
+    return gulp.src('').pipe(shell([
+        'gitbook install'    
+    ])) 
+});
+
+//------------------------------------------------------------------------------------
+
+// Generate-Gitbook
+
+gulp.task('deploy', function(){
+    return gulp.src(path.join(__dirname,'scripts'))
+       .pipe(shell(['./scripts/losh generate-gitbook']))
+       .pipe(shell(['./scripts/losh generate-wiki']))
+       .pipe(shell(['./scripts/losh deploy-gitbook']))
+       .pipe(shell(['./scripts/losh deploy-wiki']));
+});
+
+//------------------------------------------------------------------------------------
+
+gulp.task('default', ['deploy']);
