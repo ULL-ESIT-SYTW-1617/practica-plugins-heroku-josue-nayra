b447cfd8481805bb93086cb776d9de8941837acd
josuetc94 <alu0100763492@ull.edu.es>
2016-10-25 12:08:23 +0000

    Actualizando paquete

--- bin/gitbook-start.js
+++ bin/gitbook-start.js
@@ -33,20 +33,21 @@ else
             const packagejson = require(path.join(basePath, 'package.json'));
             console.log("Desplegando en... "+myArgs.deploy);
             var dependencias = packagejson.dependencies;
-            var dependencia_buscada;
+            // var dependencia_buscada;
             
             for (var d in dependencias){
                 if(d.search(myArgs.deploy) != -1){
-                    dependencia_buscada = d;  
-                    var tarea_gulp = `\n\ngulp.task("deploy-${myArgs.deploy}", function(){`+
-                             `\n       require("${dependencia_buscada}").deploy("${myArgs.IP}", "${myArgs.path}", "${packagejson.repository.url}", "${myArgs.usuarioremoto}");`+
-                             `\n});`;     
-                    fs.appendFile(path.join(basePath,'gulpfile.js'), `${tarea_gulp}`, (err) => {
-                      if (err) throw err;
-                        console.log("Escribiendo tarea en gulpfile para próximos despliegues");        
-                    });
+                    // dependencia_buscada = d;  
+                    // var tarea_gulp = `\n\ngulp.task("deploy-${myArgs.deploy}", function(){`+
+                    //          `\n       require("${dependencia_buscada}").deploy("${myArgs.IP}", "${myArgs.path}", "${packagejson.repository.url}", "${myArgs.usuarioremoto}");`+
+                    //          `\n});`;     
+                    // fs.appendFile(path.join(basePath,'gulpfile.js'), `${tarea_gulp}`, (err) => {
+                    //   if (err) throw err;
+                    //     console.log("Escribiendo tarea en gulpfile para próximos despliegues");        
+                    // });
                     
-                    require(d).deploy(myArgs.IP, myArgs.path, packagejson.repository.url, myArgs.usuarioremoto);
+                    // require(d).deploy(myArgs.IP, myArgs.path, packagejson.repository.url, myArgs.usuarioremoto);
+                    require(d).initialize(myArgs.IP,myArgs.path,packagejson.repository.url,myArgs.usuarioremoto);
                     break;
                 }
             }
